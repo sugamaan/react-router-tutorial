ba165e6aa1ea91fc0f927f1a5857e4b18b9296c0
sugama <reiji.sugama@gmail.com>
2024-07-21 14:11:41 +0900

    ルートルートの追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Root from "./routes/root";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <div>Hello world!</div>,
-});
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
