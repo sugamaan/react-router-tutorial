f31383ad84d4c28b611e3d4fa84a0dda2dd9fb66
sugama <reiji.sugama@gmail.com>
2024-07-21 14:24:26 +0900

    ネストされたルートまで追加

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "contacts/:contactId",
